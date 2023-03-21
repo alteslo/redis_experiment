1b192974bee651bbd876980e83451c8276046d05
Teslenko Aleksandr <alteslo31@gmail.com>
2023-03-21 18:43:05 +0300

    del aioredis

--- forth_app.py
+++ forth_app.py
@@ -1,83 +1,42 @@
 import asyncio
 
 import json
-from aioredis import Redis
-from aioredis.client import PubSub
-from fastapi import FastAPI, BackgroundTasks
-from fastapi.params import Depends
-from fastapi_plugins import depends_redis, redis_plugin
-from sse_starlette.sse import EventSourceResponse
-from fastapi_utils.tasks import repeat_every
+from fastapi import FastAPI
 import async_timeout
-# from starlette.responses import HTMLResponse
-
+import redis.asyncio as redis
+from redis.client import PubSub
 
 STOPWORD = "STOP"
 
 app = FastAPI()
+client = redis.from_url('redis: // localhost')
 
 
 @app.on_event("startup")
 async def on_startup() -> None:
-    await redis_plugin.init_app(app)
-    await redis_plugin.init()
-    subscriber = redis_plugin.redis.pubsub()
-    # loop = asyncio.get_event_loop()
+    subscriber = client.pubsub()
     asyncio.create_task(consumer("first_app:channel", subscriber))
-    # await loop.run_forever()
 
 
 @app.on_event("shutdown")
 async def on_shutdown() -> None:
-    await redis_plugin.terminate()
+    await client.close()
 
 
 @app.get("/")
-async def root_get(cache: Redis = Depends(depends_redis)) -> dict:
-    return dict(ping=await cache.ping())
-
-
-# async def consumer(channel: str, pub_sub: PubSub):
-#     async with pub_sub as p:
-#         await p.subscribe(channel)
-#         while True:
-#             try:
-#                 async with async_timeout.timeout(1):
-#                     message = await p.get_message(ignore_subscribe_messages=True)
-#                     if message:
-#                         # if message.get('data') == STOPWORD:
-#                         #     break
-#                         print({"event": "message", "data": message.get('data')})
-#                         # yield {"event": "message", "data": message.get('data')}
-#                     await asyncio.sleep(0.01)
-#             except asyncio.TimeoutError:
-#                 pass
+async def root_get() -> dict:
+    return dict(ping=await client.ping())
 
 
 async def consumer(channel: str, pub_sub: PubSub):
-    async with pub_sub as p:
-        await p.subscribe(channel)
-        while True:
-            try:
-                async with async_timeout.timeout(1):
-                    message = await p.get_message(ignore_subscribe_messages=True)
-                    if message:
-                        message = json.loads(message.get('data'))
-                        print({"event": "message", "data": message})
-                    await asyncio.sleep(0.01)
-            except asyncio.TimeoutError:
-                pass
-
-
-# @repeat_every(seconds=0.01)
-# async def consumer_th(channel: str, pub_sub: PubSub):
-#     async with pub_sub as p:
-#         await p.subscribe(channel)
-#         try:
-#             async with async_timeout.timeout(1):
-#                 message = await p.get_message(ignore_subscribe_messages=True)
-#                 if message:
-#                     print({"event": "message", "data": message.get('data')})
-#                     # yield {"event": "message", "data": message.get('data')}
-#         except asyncio.TimeoutError:
-#             pass
+    await pub_sub.psubscribe(channel)
+    while True:
+        try:
+            async with async_timeout.timeout(1):
+                message = await pub_sub.get_message(ignore_subscribe_messages=True)
+                if message:
+                    message = json.loads(message.get('data'))
+                    print({"event": "message", "data": message})
+                await asyncio.sleep(0.01)
+        except asyncio.TimeoutError:
+            pass
