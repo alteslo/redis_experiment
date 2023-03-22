85a2a3867d9655ea59574e04d62b9c912262ae13
Teslenko Aleksandr <alteslo31@gmail.com>
2023-03-22 12:14:47 +0300

    fix

--- forth_app.py
+++ forth_app.py
@@ -6,42 +6,52 @@ import async_timeout
 import redis.asyncio as redis
 from redis.client import PubSub
 
-STOPWORD = "STOP"
+SUBSCRIBE_CHANNELS = ('first_app:channel', 'test_app:channel')
 
 app = FastAPI()
 client = redis.from_url('redis: // localhost')
 
 
-@app.on_event("startup")
+@app.on_event('startup')
 async def on_startup() -> None:
-    asyncio.create_task(consumer("first_app:channel"))
+    asyncio.create_task(listen(SUBSCRIBE_CHANNELS))
 
 
-@app.on_event("shutdown")
+@app.on_event('shutdown')
 async def on_shutdown() -> None:
     await client.close()
 
 
-@app.get("/")
+@app.get('/')
 async def root_get() -> dict:
     return dict(ping=await client.ping())
 
 
-async def get_pubsub(channel: str):
+async def get_pubsub(channels):
     subscriber = client.pubsub()
-    await subscriber.psubscribe(channel)
+    for channel in channels:
+        await subscriber.psubscribe(channel)
     return subscriber
 
 
-async def consumer(channel: str):
-    pub_sub: PubSub = await get_pubsub(channel)
+async def message_manager(message: dict | None):
+    if message:
+        channel: str = message.get('channel').decode('utf-8')
+        message: dict = json.loads(message.get('data'))
+        match channel:
+            case 'first_app:channel':
+                print({'channel': 'first_app:channel', 'data': message})
+            case 'test_app:channel':
+                print({'channel': 'Тестовый канал', 'data': message})
+
+
+async def listen(*channels):
+    subscriber: PubSub = await get_pubsub(*channels)
     while True:
         try:
             async with async_timeout.timeout(1):
-                message = await pub_sub.get_message(ignore_subscribe_messages=True)
-                if message:
-                    message = json.loads(message.get('data'))
-                    print({"event": "message", "data": message})
+                message = await subscriber.get_message(ignore_subscribe_messages=True)
+                await message_manager(message)
                 await asyncio.sleep(0.01)
         except asyncio.TimeoutError:
             pass
